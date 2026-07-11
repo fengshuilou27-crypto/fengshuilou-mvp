8aad0e8765d09179116ef5e43d2360f5383762eb
fengshuilou27-crypto <fengshuilou27@gmail.com>
2026-07-11 17:44:00 +0800

    cleanup: remove temporary extraction files

--- run_extract.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-import json
-
-# Force UTF-8 for stdout
-sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
-
-# Run the extraction script
-exec(open('extract_professional_data.py', encoding='utf-8').read())
